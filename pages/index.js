ad6b43ad9e0cfc5a8f38404f4d0e2a6388bb147f
nykoh2001 <nykoh2001@gmail.com>
2023-05-13 16:25:58 +0900

    feat: pages

---
+++ pages/index.js
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+
+export default function Home({ results }) {
+	return (
+		<div>
+			<h1>Home</h1>
+			<div className="container">
+				{results?.map((movie) => {
+					return (
+						<div key={movie.id} className="movie">
+							<img
+								src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+							/>
+							<h4>{movie.original_title}</h4>
+						</div>
+					);
+				})}
+			</div>
+			<style jsx>{`
+				.container {
+					display: grid;
+					grid-template-columns: 1fr 1fr;
+					padding: 20px;
+					gap: 20px;
+				}
+				.movie img {
+					max-width: 100%;
+					border-radius: 12px;
+					transition: transform 0.2s ease-in-out;
+					box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+				}
+				.movie:hover img {
+					transform: scale(1.05) translateY(-10px);
+				}
+				.movie h4 {
+					font-size: 18px;
+					text-align: center;
+				}
+			`}</style>
+		</div>
+	);
+}
+
+export async function getServerSideProps() {
+	const { results } = await (
+		await fetch("http://localhost:3000/api/movies")
+	).json();
+	return {
+		props: {
+			results,
+		},
+	};
+}
